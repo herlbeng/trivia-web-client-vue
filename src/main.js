0b2fa692f8bbd1a05b772045d2f12a91930a80b9
Hermann <hbenda.siap@gmail.com>
2019-09-20 06:58:40 -0500

    all

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+import Dashboard from './components/Dashboard.vue'
+import TriviaGame from './components/TriviaGame.vue'
+import Auth from '@okta/okta-vue'
+
+
+const routes = [
+  { path: '/trivia', component: TriviaGame }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(Dashboard)
 }).$mount('#app')
