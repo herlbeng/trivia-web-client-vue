4ba65a99f22cb46915a2927855a87f8954b30392
Hermann <hbenda.siap@gmail.com>
2019-09-21 14:04:10 -0500

    configurando rutas en aws

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const API_BASE_URL = 'http://localhost:8000/api';
+//export const API_BASE_URL = 'http://localhost:8000/api';
+export const API_BASE_URL = 'http://triviawebservice-env.fjmdpc3wc3.us-east-2.elasticbeanstalk.com/api';
 export const TRIVIA_ENDPOINT = 'http://jservice.io/api/random?count=1';
--- src/main.js
+++ src/main.js
@@ -11,7 +11,8 @@ import Auth from '@okta/okta-vue'
 Vue.use(Auth, {
   issuer: 'https://dev-393921.okta.com/oauth2/default',
   client_id: '0oa1dxgwde3RfEqVb357',
-  redirect_uri: 'http://localhost:8081/implicit/callback',
+  //redirect_uri: 'http://localhost:8081/implicit/callback',
+  redirect_uri: 'https://master.d15cf51swd0vqo.amplifyapp.com/implicit/callback',
   scope: 'openid profile email'
 })
 
